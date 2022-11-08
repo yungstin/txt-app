74f8912eef291badee7fe9a9bb98e149e8d409f4
yungstin <justisl9@uci.edu>
2022-11-08 15:50:57 -0800

    v1

---
+++ app/src/main/java/com/example/txtapp/DailyJournal.kt
@@ -0,0 +1,27 @@
+package com.example.txtapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class DailyJournal : AppCompatActivity() {
+    private lateinit var dailyjournaledt: EditText
+    private lateinit var returnbtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dailyjournal)
+
+        dailyjournaledt = findViewById(R.id.dailyjournal)
+
+        returnbtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/txtapp/MainActivity.kt
+++ app/src/main/java/com/example/txtapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.txtapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btndailyjournal: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btndailyjournal = findViewById(R.id.dailyjournal)
+
+        btndailyjournal.setOnClickListener {
+            val intent = Intent(this, DailyJournal::class.java)
+            startActivity(intent)
+        }
+
+//        btnsignup.setOnClickListener{
+//            val intent = Intent(this, SignUp::class.java)
+//            startActivity(intent)
+//        }
     }
 }
