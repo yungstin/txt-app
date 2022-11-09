cf79bfe20d494c40c339cbe965bbcc40bd34bcfb
yungstin <justisl9@uci.edu>
2022-11-08 22:07:18 -0800

    database 1

--- app/src/main/java/com/example/txtapp/DailyJournal.kt
+++ app/src/main/java/com/example/txtapp/DailyJournal.kt
@@ -5,20 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class DailyJournal : AppCompatActivity() {
     private lateinit var dailyjournaledt: EditText
     private lateinit var returnbtn: Button
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dailyjournal)
 
         dailyjournaledt = findViewById(R.id.journalentry)
-        returnbtn = findViewById(R.id.returnjournal)
+        returnbtn = findViewById(R.id.saveButton)
+
+        val dailyentry = dailyjournaledt.text.toString()
 
 
         returnbtn.setOnClickListener {
+            var database = Firebase.database
+            val myRef = database.getReference("Journal Entry")
+            myRef.setValue(dailyentry)
+
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
